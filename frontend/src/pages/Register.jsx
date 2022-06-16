da90a823802873d7f3ebd251a1f5d62c0a793718
Adeesha97 <adeeshakawshal1997@gmail.com>
2022-06-16 16:05:26 +0530

    add login

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -36,7 +36,7 @@ function Register() {
     }
 
     dispatch(reset());
-  }, [user, isError, isSuccess, message]);
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
